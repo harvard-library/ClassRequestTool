9bdb10630bbff2c493d242bb0037582eb2926ebf
Anita Patel <anita@anitapatel.com>
2013-03-18 21:23:25 -0400

    static assets

--- config/application.rb
+++ config/application.rb
@@ -54,8 +54,8 @@ module ClassRequestTool
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
+    config.assets.enabled = false
+    #config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
