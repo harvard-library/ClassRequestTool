98169011a182f0f429136908684624eddb48b8b2
Anita Patel <anita@anitapatel.com>
2013-03-18 22:33:26 -0400

    static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ ClassRequestTool::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-  #config.assets.precompile += %w( bootstrap.css )
+  config.assets.compile = false
+  config.assets.precompile += %w( jquery-ui-timepicker-addon.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
