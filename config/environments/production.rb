7db18be2a8bcb9e948d66141e4ea0fe434731f27
Anita <anita@anitapatel.com>
2013-05-22 13:32:16 -0400

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,9 @@ ClassRequestTool::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.default_url_options = { :host => 'classrequest.dev.berkmancenter.org' }
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable threaded mode
   # config.threadsafe!
