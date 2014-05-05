5a478bd3287223aea005694aa79d77215087f44e
Dave Mayo <dave_mayo@harvard.edu>
2014-05-05 16:17:03 -0400

    LTSCLASREQ-12 - Minimal Putative Fix.  Also translated into Rails3-style AR.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,24 @@
 class WelcomeController < ApplicationController
   before_filter :authenticate_admin_or_staff!, :only => [:dashboard]
-  
-  def index   
-    @repositories = Repository.find(:all, :order => :name)
-    @courses = Array.new
-    @courses = Course.find(:all, :conditions => ["timeframe is not NULL and timeframe < ?", DateTime.now], :order => "timeframe DESC", :limit => 10)
-  end  
-  
-  def dashboard    
+
+  def index
+    @repositories = Repository.order(:name)
+    @courses = Course.where("timeframe IS NOT NULL AND status = 'Closed' AND timeframe < ?", DateTime.now).order('timeframe DESC').limit(10)
+  end
+
+  def dashboard
     @homeless = Course.homeless
     @unscheduled_unclaimed = Course.unscheduled_unclaimed
     @scheduled_unclaimed = Course.scheduled_unclaimed
-    
+
     @your_upcoming = current_user.upcoming_courses
     @your_past = current_user.past_courses
     @your_unscheduled = current_user.unscheduled_courses
     @your_repo_courses = current_user.upcoming_repo_courses
     @your_classes_to_close = current_user.classes_to_close
   end
-  
-  def dashboard_items      
+
+  def dashboard_items
     course_ids = params[:courses]
     @title = params[:title]
     unless course_ids.nil?
