58b47da7a393930a43064a192328bf68092a7d7d
Dave Mayo <dave_mayo@harvard.edu>
2014-06-02 10:24:07 -0400

    Missing association.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ActiveRecord::Base
   attr_accessible :location_id, :name, :repository_ids, :item_attribute_ids, :class_limit
-  
+
+  belongs_to :location
   has_and_belongs_to_many :repositories
   has_many :courses
   has_and_belongs_to_many :item_attributes
