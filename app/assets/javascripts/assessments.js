f698c07d8679246c2057a4db20983ab04b9068e0
Dave Mayo <dave_mayo@harvard.edu>
2014-06-10 17:24:09 -0400

    Popover management - close on click elsewhere

--- app/assets/javascripts/assessments.js
+++ app/assets/javascripts/assessments.js
@@ -1,6 +1,22 @@
 $(function () {
-  $('#assessment-table-current button[data-toggle="popover"]').popover();
-  $('#assessment-table-current').on('click', 'button[data-toggle="popover"]', function (e) {
-    $('#assessment-table-current button[data-toggle="popover"]').not(e.target).popover('hide');
-  })
+  var popover_sel = '#assessment-table-current button[data-toggle="popover"]'
+  /* assessments#index */
+  if (window.location.href.match(/assessments$/)) {
+    $(popover_sel).popover();
+
+    $('#assessment-table-current').on('click', 'button[data-toggle="popover"]', function (e) {
+      $(document).off('click.clearPopUps');
+      $(popover_sel).not(e.target).popover('hide');
+      $(document).one('click.clearPopUps', function (e) {
+        $(popover_sel).popover('hide');
+      });
+      e.stopPropagation(); /* stops at table */
+    });
+
+    /* Stop click events on the popup from bubbling (and triggering clear) */
+    $('body').on('click', '.popover', function (e) {
+      e.stopPropagation();
+    });
+
+  }
 });
