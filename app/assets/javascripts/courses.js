b079524ff8caa611be067e758f10937dca964d8c
Dave Mayo <dave_mayo@harvard.edu>
2014-06-20 15:55:08 -0400

    Inching ever closer to actually working!

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,11 +1,10 @@
 $(function () {
   /* courses#(new|edit) */
   if (window.location.href.match(/courses\/(\d+\/)?(new|edit)(?:\?.+)?/)){
+    /* Sets a particular requested date as actual date */
     $('body').on('click', 'button.date-setter', function (e) {
       $(this).closest('.section').find('input.actual-date').val($(this).prev().find('input').val());
-      e.stopPropagation();
       e.preventDefault();
-      return false;
     });
   }
 });
