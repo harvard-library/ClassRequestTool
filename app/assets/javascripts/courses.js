7aec5f6d3347e1c15534329b7612953109b07e5a
Dave Mayo <dave_mayo@harvard.edu>
2014-07-23 10:19:37 -0400

    Base whether you can delete or not on number of undeleted sessions/sections, not position.

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -47,20 +47,39 @@ $(function () {
 
       var $this_session = $(e.currentTarget).closest('.session');
       var persisted = $this_session.find('.id_val').length;
+      var last = ($('.session:not(.deleted)').length == 1)
 
-      if (persisted) {
-        $this_session.addClass('deleted');
-        $this_session.find('.section input.id_val').each(function (i, el) {
-          var name = $(el).attr('name').replace(/\[id\]$/, '[_destroy]');
-          $(el).closest('.section').append('<input type="hidden" name="' + name + '" value="1">');
-        });
-      }
-      else {
-        if ($('.session').length > 1) {
-          $this_session.remove();
+      if (!last) {
+        if (persisted) {
+          $this_session.addClass('deleted');
+          $this_session.find('button.delete_session').text('+');
+          $this_session.find('.section input.id_val').each(function (i, el) {
+            var name = $(el).attr('name').replace(/\[id\]$/, '[_destroy]');
+            $(el).closest('.section').append('<input type="hidden" class="destroy" name="' + name + '" value="1">');
+          });
+        }
+        else {
+          if ($('.session').length > 1) {
+            $this_session.remove();
+          }
         }
       }
+      else {
+        alert("Cannot delete last session on page");
+      }
     });
+
+    /* Undelete session */
+    $('body').on('click', '.session.deleted button.delete_session', function (e) {
+      e.preventDefault();
+
+      var $this_session = $(e.currentTarget).closest('.session');
+
+      $this_session.find('input.destroy').remove();
+      $this_session.removeClass('deleted');
+      $this_session.find('button.delete_session').text('-');
+    })
+
 
     /* Delete sections, either by adding _destroy input (for sections in DB) *
      *   or by deleting section from the page if not.                        *
@@ -72,15 +91,33 @@ $(function () {
       var $section = $(e.currentTarget).closest('.section');
 
       var persisted = $section.find('.id_val').length;
+      var last = ($this_session.find('.section:not(.deleted)').length == 1)
       var name = $section.find('input.id_val').attr('name').replace(/\[id\]$/, '[_destroy]');
 
-      if (persisted) {
-        $section.addClass('deleted');
-        $section.append('<input type="hidden" name="' + name + '" value="1">');
+      if (!last) {
+        if (persisted) {
+          $section.addClass('deleted');
+          $section.find('button.delete_section').text('+');
+          $section.append('<input type="hidden" class="destroy" name="' + name + '" value="1">');
+        }
+        else {
+          $section.remove();
+        }
       }
       else {
-        $section.remove();
+        alert("Cannot delete last section from session");
       }
+    });
+
+    /* Undelete section */
+    $('body').on('click', '.section.deleted button.delete_section', function (e) {
+      e.preventDefault();
+
+      var $section = $(e.currentTarget).closest('.section');
+
+      $section.find('input.destroy').remove();
+      $section.removeClass('deleted');
+      $section.find('button.delete_section').text('-');
     });
   }
 });
