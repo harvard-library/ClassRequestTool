82ee2ec48afa45e2f414559f0094bb21845505e5
Dave Mayo <dave_mayo@harvard.edu>
2014-08-13 14:24:04 -0400

    LTSCLASREQ-46 - Contact name added to table, also default number of rows adjusted.

--- app/assets/javascripts/tableSorters.js
+++ app/assets/javascripts/tableSorters.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     var id = '#' + name;
     var pg_id = '#pager-' + name;
     var search_id = name + '_search';
+
+    var ts_defaults = {widgets:['filter'], widgetOptions: {filter_external: '#' + search_id, filter_columnFilters: false}}
+    var pg_defaults = {container: $(pg_id), removeRows: false, output: "{page}/{filteredPages} pages ({totalPages} total)", size: 20}
+
     // Create search field
     $(id).before('<input id="' + search_id + '" type="search" class="search" data-column="all" placeholder="Search table" />');
 
-    $(id).tablesorter($.extend({widgets:['filter'], widgetOptions: {filter_external: '#' + search_id, filter_columnFilters: false}}, opts))
-      .tablesorterPager($.extend({container: $(pg_id), removeRows: false, output: "{page}/{filteredPages} pages ({totalPages} total)"}, pg_opts));
+    $(id).tablesorter($.extend(ts_defaults, opts))
+      .tablesorterPager($.extend(pg_defaults, pg_opts));
   }
 
   prepTablesorter('course-table-current');
@@ -17,17 +21,17 @@ $(document).ready(function(){
   prepTablesorter('locations-table');
   prepTablesorter('repositories-table');
   prepTablesorter('rooms-table');
-  prepTablesorter('users-table', {}, {size: 40});
+  prepTablesorter('users-table');
   prepTablesorter('involvement-table');
-  prepTablesorter('your-unscheduled-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('your-unscheduled-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('your-upcoming-table', {}, {size: 5, positionFixed:false});
-  prepTablesorter('your-past-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('homeless-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('unassigned-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('roomless-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('your-repo-courses-table', {}, {size: 5, positionFixed: false});
-  prepTablesorter('your-to-close-table', {}, {size: 5, positionFixed: false});
+  prepTablesorter('your-unscheduled-table', {}, {positionFixed: false});
+  prepTablesorter('your-unscheduled-table', {}, {positionFixed: false});
+  prepTablesorter('your-upcoming-table', {}, {positionFixed:false});
+  prepTablesorter('your-past-table', {}, {positionFixed: false});
+  prepTablesorter('homeless-table', {}, {positionFixed: false});
+  prepTablesorter('unassigned-table', {}, {positionFixed: false});
+  prepTablesorter('roomless-table', {}, {positionFixed: false});
+  prepTablesorter('your-repo-courses-table', {}, {positionFixed: false});
+  prepTablesorter('your-to-close-table', {}, {positionFixed: false});
 
   $("#course-table").tablesorter();
   $('#assessment-table-current').tablesorter();
