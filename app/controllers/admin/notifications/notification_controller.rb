adc0d962aafa5e565995c0462d6d171557d11ec8
Tim Kinnel <tkinnel@gmail.com>
2014-12-09 15:11:24 -0500

    Admin display of notifications

--- app/controllers/admin/notifications/notification_controller.rb
+++ app/controllers/admin/notifications/notification_controller.rb
@@ -1,41 +1,44 @@
 class Admin::Notifications::NotificationController < ApplicationController
-  def assessment()
-    @course = Course.where(status: 'Closed').last
-    render :file => 'notification/assessment', :layout => 'notification'
-  end
+
+  # By default list links to all emails
+  def method_missing(m, *args, &block)
   
-  def cancellation()
-    @course = Course.where("status != 'Cancelled'").last
-    render :file => 'notification/cancellation', :layout => 'notification'
-  end
-  
-  def new_request_to_admin()
-    @course = Course.where("status != 'Homeless'").last
-    render :file => 'notification/new_request_to_admin', :layout => 'notification'
-  end
-  
-  def new_request_to_requestor()
-    @course = Course.where("status != 'Homeless'").last
-    render :file => 'notification/new_request_to_requestor', :layout => 'notification'
-  end
-  
-  def repo_change()
-    @course = Course.where(status: 'Scheduled, Claimed').last
-    render :file => 'notification/repo_change', :layout => 'notification'
-  end
-  
-  def staff_change()
-    @course = Course.where(status: 'Scheduled, Claimed').last
-    render :file => 'notification/staff_change', :layout => 'notification'
-  end
-  
-  def timeframe_change()
-    @course = Course.where(status: 'Scheduled, Claimed').last
-    render :file => 'notification/timeframe_change', :layout => 'notification'
-  end
-  
-  def uncancellation()
-    @course = Course.where(status: 'Cancelled').last
-    render :file => 'notification/uncancellation', :layout => 'notification'
+    case (m)
+      when :assessment
+        @course = Course.where(status: 'Closed').last
+        render :file => 'notification/assessment', :layout => 'notification_preview'
+
+      when :cancellation
+        @course = Course.where("status != 'Cancelled'").last
+        render :file => 'notification/cancellation', :layout => 'notification_preview'
+
+      when :new_request_to_admin
+        @course = Course.where("status != 'Homeless'").last
+        render :file => 'notification/new_request_to_admin', :layout => 'notification_preview'
+
+      when :new_request_to_requestor
+        @course = Course.where("status != 'Homeless'").last
+        render :file => 'notification/new_request_to_requestor', :layout => 'notification_preview'
+
+      when :repo_change
+        @course = Course.where(status: 'Scheduled, Claimed').last
+        render :file => 'notification/repo_change', :layout => 'notification_preview'
+
+      when :staff_change
+        @course = Course.where(status: 'Scheduled, Claimed').last
+        render :file => 'notification/staff_change', :layout => 'notification_preview'
+
+      when :timeframe_change
+        @course = Course.where(status: 'Scheduled, Claimed').last
+        render :file => 'notification/timeframe_change', :layout => 'notification_preview'
+
+      when :uncancellation
+        @course = Course.where(status: 'Cancelled').last
+        render :file => 'notification/uncancellation', :layout => 'notification_preview'
+        
+      else
+        render :file => 'admin/notifications/all_notifications'
+
+    end 
   end
 end
