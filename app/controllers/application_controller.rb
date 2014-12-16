cdb5c0bf542cf9442ee6ea995c6eecc81d01ec2a
Tim Kinnel <tkinnel@gmail.com>
2014-12-15 20:50:25 -0500

    Added cookie to save login destination (necessary because of intermediary page where one chooses username or PIN signin) and Devise's after_sign_in_path_for method in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   #before_filter :authenticate_user!
   protect_from_forgery
   
+  def after_sign_in_path_for(user)
+    cookies[:login_destination]
+  end
+  
   private 
   def authenticate_login!
     if !user_signed_in?
+      cookies[:login_destination] = request.path
       redirect_to(login_welcome_index_url)
     end  
   end
