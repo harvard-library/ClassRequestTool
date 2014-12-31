86014ce13d6d044cbb92ec7ef55ade9a6ccecd83
Tim Kinnel <tkinnel@gmail.com>
2014-12-30 23:38:59 -0500

    Add note text validation so empty notes are not created.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,8 @@ class Note < ActiveRecord::Base
   include ActionDispatch::Routing::UrlFor
   include Rails.application.routes.url_helpers
   attr_accessible :note_text, :user_id, :course_id, :staff_comment
+  
+  validates :note_text, :presence => true, length: { minimum: 3 }
 
   belongs_to :user
   belongs_to :course
