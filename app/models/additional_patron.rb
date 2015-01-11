d3a1efe5a4bb2fc0e40ed2f45f69dbb1d8157a4a
Tim Kinnel <tkinnel@gmail.com>
2015-01-10 19:08:36 -0500

    Adding the addtional patrons in the course request.

--- app/models/additional_patron.rb
+++ app/models/additional_patron.rb
@@ -1,5 +1,5 @@
 class AdditionalPatron < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :email, :course_id
+  attr_accessible :first_name, :last_name, :email, :role, :course_id
   belongs_to :course
   validates_format_of :email, with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
 end
