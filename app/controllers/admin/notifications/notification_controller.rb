239d398320792ddc650232654b4ec3f5391e41bd
Tim Kinnel <tkinnel@gmail.com>
2015-03-24 14:07:02 -0400

    Make notifications email tests more flexible - now only requires one repository in the database

--- app/controllers/admin/notifications/notification_controller.rb
+++ app/controllers/admin/notifications/notification_controller.rb
@@ -3,37 +3,36 @@ class Admin::Notifications::NotificationController < Admin::AdminController
   # By default list links to all emails
   def action_missing(m, *args, &block)
   
+    @test = true
+  
     case (m)
       when 'assessment_received_to_admins'
-        @assessment = Assessment.last
+        @assessment = test_assessment
 
       when 'assessment_received_to_users'
-        @assessment = Assessment.last
+        @assessment = test_assessment
 
       when 'assessment_requested'
-        @course = Course.where(status: 'Closed').last
+        @course = test_course('Closed')
 
       when 'cancellation'
-        @course = Course.where("status != 'Cancelled'").last
+        @course = test_course('Active')
         
       when 'homeless_courses_reminder'
-        @courses = Course.homeless.limit(3).includes(:repository)
-        if @courses.empty?
-          @courses = Course.unclaimed.limit(3).includes(:repository)
-        end
+        @courses = [test_course('Active', true)]
 
       when 'new_note'
-        @course = Course.where("status = 'Active'").last
+        @course = test_course('Active')
         @note = Note.new(:staff_comment => false, :note_text => 'I have something to say about this!')
 
       when 'new_request_to_admin', 'new_request_to_requestor'
-        @course = Course.where("status != 'Homeless'").last
+        @course = test_course('Active')
 
       when 'uncancellation'
-        @course = Course.where(status: 'Cancelled').last
+        @course = test_course('Cancelled')
 
       when 'repo_change', 'staff_change', 'timeframe_change'
-        @course = Course.where(status: 'Active').last
+        @course = test_course('Active')
         
       when ':action'
         all_notifications = 'admin/notifications/all_notifications'
@@ -72,4 +71,44 @@ class Admin::Notifications::NotificationController < Admin::AdminController
       render :nothing
     end
   end
+  
+  private
+    def test_assessment
+      assessment = Assessment.new({
+        using_materials: "This is some text", 
+        involvement: "This is some text", 
+        staff_experience: 2, 
+        staff_availability: 2, 
+        space: 2, 
+        request_course: 2, 
+        request_materials: 2, 
+        catalogs: 2, 
+        digital_collections: 2, 
+        involve_again: 'No', 
+        not_involve_again: 'This is some text', 
+        better_future: 'This is what you could do better', 
+        comments: 'These are my comments'
+      })
+      assessment.course = test_course('Closed')
+      assessment
+    end
+    
+    def test_course(status, homeless = false)
+      course = Course.new({
+        title: "Test Course Title", 
+        contact_email: 'test@example.com', 
+        contact_phone: '617-555-1234', 
+        number_of_students: 10, 
+        duration: '1.5', 
+        goal: 'This is my goal', 
+        contact_first_name: 'Zelda', 
+        contact_last_name: 'Fitzgerald', 
+        contact_username: 'ziffy',
+        status: status
+      })
+      unless status == 'Homeless'
+        course.repository = Repository.first
+      end
+      course
+    end
 end
