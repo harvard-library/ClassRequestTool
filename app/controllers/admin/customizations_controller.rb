aafe020ebdb6dd13ce03dfed51b58650c696fc09
Tim Kinnel <tkinnel@gmail.com>
2015-08-02 12:51:56 -0400

    Improve customization and initialization

--- app/controllers/admin/customizations_controller.rb
+++ app/controllers/admin/customizations_controller.rb
@@ -3,7 +3,7 @@ class Admin::CustomizationsController < Admin::AdminController
   def update
     @custom = Customization.find(params[:id])
     respond_to do |format|
-      if @custom.update_attributes(params[:customization])
+      if @custom.update_attributes(customization_params)
         format.html { redirect_to admin_localize_path, notice: 'Local customization was successfully updated.' }
         format.json { head :no_content }
       else
@@ -15,4 +15,21 @@ class Admin::CustomizationsController < Admin::AdminController
       end
     end
   end
+  
+  private
+    def customization_params
+      params.require(:customization).permit(
+        :institution,
+        :institution_long,
+        :tool_name,
+        :slogan,
+        :tool_tech_admin_name,
+        :tool_tech_admin_email,
+        :tool_content_admin_name,
+        :tool_content_admin_email,
+        :default_email_sender,
+        :collaboration_options
+      )
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,48 @@
+# Basic file for seeding the database
+
+# Create a default superadmin user
+User.create(
+  :username               => 'superadmin',
+  :password               => 'password',
+  :password_confirmation  => 'password',
+  :email                  => 'superadmin@example.edu',
+  :first_name             => 'Super',
+  :last_name              => 'Admin',
+  :superadmin             => true
+)
+
+# Create a default admin user
+User.create(
+  :username               => 'admin',
+  :password               => 'password',
+  :password_confirmation  => 'password',
+  :email                  => 'admin@example.edu',
+  :first_name             => 'Merely',
+  :last_name              => 'Admin',
+  :admin                  => true
+)
+
+# Create a basic customization - this should be changed and a banner added
+Admin::Customization.create(
+  :institution              => 'Harvard',
+  :institution_long         => 'Harvard University',
+  :tool_name                => 'Class Request Tool',
+  :slogan                   => 'We have what you want to see',
+  :tool_tech_admin_name     => 'Penelope Infotech',
+  :tool_tech_admin_email    => 'infotech@example.edu',
+  :tool_content_admin_name  => 'Percival Content-Guru',
+  :tool_content_admin_email => 'content_admin@example.edu',
+  :default_email_sender     => 'library@example.edu',
+  :notifications_on         => false,
+  :collaboration_options    => [
+    'Share planning and selection responsibilities',
+    'Share instructional responsibilities',
+    'Share materials'
+  ]
+  
+)
+
+# Custom text which is inserted into notification emails. See the notification previews.  
 custom_notification_texts = {
   'assessment_received_to_admins' => '<p>Replace custom text here or delete</p>',
   'assessment_received_to_users'  => '<p>Replace custom text here or delete</p>',
