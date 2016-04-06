1c0c2c3a052e4e80a334b107ea495618e998b96a
bobbi-SMR <bobbi_fox@harvard.edu>
2016-04-06 16:31:44 -0400

    use Strong Parameters

--- app/controllers/admin/affiliates_controller.rb
+++ app/controllers/admin/affiliates_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AffiliatesController < Admin::AdminController
 
   def create
-    @affiliate = Affiliate.new(params[:affiliate])
+    @affiliate = Affiliate.new(affiliate_params)
     
     if @affiliate.save
       render :json => @affiliate.to_json
@@ -11,9 +11,9 @@ class Admin::AffiliatesController < Admin::AdminController
   end
   
   def update
-    @affiliate = Affiliate.find(params[:id])
+    @affiliate = Affiliate.find(affiliate_params[:id])
     
-    if @affiliate.update_attributes(params[:affiliate])
+    if @affiliate.update_attributes(affiliate_params)
       render :json => @affiliate.to_json
     else
       render json: @affiliate.errors, status: :unprocessable_entity
@@ -22,8 +22,9 @@ class Admin::AffiliatesController < Admin::AdminController
   
   def update_positions
     @affiliates = Affiliate.all
+    af_params = affiliate_params  # we do this to just process the params once
     @affiliates.each do |a|
-      a.position = params["affiliate-#{a.id}"]
+      a.position = af_params["affiliate-#{a.id}"]
       unless a.save
         render json: { status: 'error', msg: 'There was a problem updating the affiliate order.' }
         return       
@@ -38,4 +39,24 @@ class Admin::AffiliatesController < Admin::AdminController
 
     render :json => true
   end
+  
+  private
+  def affiliate_params
+    af_params = {}
+    if params.has_key? 'affiliate'
+      af_params = params.require(:affiliate).permit(:name,:url, :description, :id)
+    end
+    # because the ordering update requires additional parameters we have an additional step
+    # looking for parameters of the type affiliates[affiliate-\d+]
+    if params.has_key? 'affiliates'
+      pas = params[:affiliates]
+      pas.each do |k,a|
+        if ( k =~ /^affiliate-\d+$/ )
+          af_params[k] = a
+        end
+      end
+    end
+    af_params
+  end
+
 end
