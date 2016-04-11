0be80a36d7bc1d8f2b75ed359eaac483a7be95b6
bobbi-SMR <bobbi_fox@harvard.edu>
2016-04-11 16:04:22 -0400

    Fix the ckeditor problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406152659) do
+ActiveRecord::Schema.define(version: 20160411152238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,22 @@ ActiveRecord::Schema.define(version: 20160406152659) do
 
   add_index "attached_images", ["featured"], name: "index_attached_images_on_featured", using: :btree
   add_index "attached_images", ["picture_id", "picture_type"], name: "index_attached_images_on_picture_id_and_picture_type", using: :btree
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
   create_table "collections", force: :cascade do |t|
     t.string   "name"
