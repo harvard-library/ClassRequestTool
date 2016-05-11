d0a3d0a7897d3f98f3b2416b95a00bb3e084215b
bobbi-SMR <bobbi_fox@harvard.edu>
2016-05-11 09:39:01 -0400

    replace nav element with div due to IE compatible modes

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,11 +5,11 @@ CKEDITOR.config.toolbarGroups = [
     { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
 ];
 $(function () {
-  $('nav.navbar li').each(function (i, el) {
+  $('#nav.navbar li').each(function (i, el) {
     var $el = $(el);
     if ($el.find('a').attr('href') === location.pathname.replace(/\/\d+(\/.+)?$/, '')){
       $el.addClass('active');
-      $el.parentsUntil('nav.navbar', 'li').addClass('active');
+      $el.parentsUntil('#nav.navbar', 'li').addClass('active');
     }
   });
   
