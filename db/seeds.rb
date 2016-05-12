7e4758eed31871f28d26d24ccd0bdcfe39acd3ee
bobbi-SMR <bobbi_fox@harvard.edu>
2016-05-12 14:58:58 -0400

    strip down seeds to essential

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,85 +1,19 @@
 # Basic file for seeding the database
 
 # Create a default superadmin user
+pw =  if %w[development test dev local].include?(Rails.env)
+        User.random_password(size = 8)
+      else
+        User.random_password
+      end 
 User.create(
   :username               => 'superadmin',
-  :password               => 'password',
-  :password_confirmation  => 'password',
+  :password               => pw,
+  :password_confirmation  => pw,
   :email                  => 'superadmin@example.edu',
   :first_name             => 'Super',
   :last_name              => 'Admin',
   :superadmin             => true
 )
-
-# Create a default admin user
-User.create(
-  :username               => 'admin',
-  :password               => 'password',
-  :password_confirmation  => 'password',
-  :email                  => 'admin@example.edu',
-  :first_name             => 'Merely',
-  :last_name              => 'Admin',
-  :admin                  => true
-)
-
-# Create a basic customization - this should be changed and a banner added
-Customization.create(
-  :institution              => 'Harvard',
-  :institution_long         => 'Harvard University',
-  :tool_name                => 'Class Request Tool',
-  :slogan                   => 'We have what you want to see',
-  :tool_tech_admin_name     => 'Penelope Infotech',
-  :tool_tech_admin_email    => 'infotech@example.edu',
-  :tool_content_admin_name  => 'Percival Content-Guru',
-  :tool_content_admin_email => 'content_admin@example.edu',
-  :default_email_sender     => 'library@example.edu',
-  :notifications_on         => false,
-  :collaboration_options    => [
-    'Share planning and selection responsibilities',
-    'Share instructional responsibilities',
-    'Share materials'
-  ]
-  
-)
-# Example repository
-Repository.create(
-  :name => 'The Example Library',
-  :description => 'This is an example of a library or archive.',
-  :class_limit => 17,
-  :landing_page => 'http://library.example.edu',
-  :class_policies => 'First, do no harm.'
-)
-
-# Example staff service
-StaffService.create(
-  :description => "Pedicure in Harvard crimson.",
-  :required => true,
-#  :global => true
-)
-
-# Example item attribute
-ItemAttribute.create(
-  :name => 'Multimedia player',
-  :description => 'This can play MP3, MP4, DVDs, and CDs'
-)
-
-
-# Custom text which is inserted into notification emails. See the notification previews.  
-custom_notification_texts = {
-  'assessment_received_to_admins' => '<p>Replace custom text here or delete</p>',
-  'assessment_received_to_users'  => '<p>Replace custom text here or delete</p>',
-  'assessment_requested'          => '<p>Replace custom text here or delete</p>',
-  'cancellation'                  => '<p>Replace custom text here or delete</p>',
-  'homeless_courses_reminder'     => '<p>Replace custom text here or delete</p>',
-  'new_note'                      => '<p>Replace custom text here or delete</p>',
-  'new_request_to_admin'          => '<p>Replace custom text here or delete</p>', 
-  'new_request_to_requestor'      => '<p>Replace custom text here or delete</p>',
-  'uncancellation'                => '<p>Replace custom text here or delete</p>',
-  'repo_change'                   => '<p>Replace custom text here or delete</p>', 
-  'staff_change'                  => '<p>Replace custom text here or delete</p>', 
-  'timeframe_change'              => '<p>Replace custom text here or delete</p>',
-}
-
-custom_notification_texts.each do |k, v|
-  Admin::CustomText.create( key: k, text: v )
-end
+puts "Superadmin USERNAME is: 'superadmin'"
+puts "Superadmin PASSWORD is: '#{pw}'"
