7d7efc371043be5429e6295e48ab5c19b02a0a45
Dave Mayo <dave_mayo@harvard.edu>
2020-12-08 20:10:33 -0500

    Called .permit! to permit arbitrary filters

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -14,6 +14,7 @@ class Admin::AdminController < ApplicationController
     filters = []
     params[:filters].each do |i, filter|
       unless filter.values.first['value'].blank?
+        filter.permit!
         filter_string = filter.to_h.collect { |k,v| "#{k[0]}.#{v['column']}#{op(v['comparison'])}#{v['value']}" }.first
         filters << filter_string
       end
